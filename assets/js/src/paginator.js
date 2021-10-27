a5bb2fa992f76d1db2e1ad566f79a071aa893405
huapeiliang <2473095423@qq.com>
2021-10-27 15:38:39 +0800

    编辑文章

--- assets/js/src/paginator.js
+++ assets/js/src/paginator.js
@@ -1,11 +1,120 @@
 'use strict';
 
 function paginatorHelper(options) {
+  // options = options || {};
+  //
+  // var current = options.current || 0;
+  // var total = options.total || 1;
+  // var endSize = options.hasOwnProperty('end_size') ? +options.end_size : 1;
+  // var midSize = options.hasOwnProperty('mid_size') ? +options.mid_size : 2;
+  // var space = options.hasOwnProperty('space') ? options.space : '&hellip;';
+  // var base = options.base || '';
+  // var format = options.format || '/%d/';
+  // var prevText = options.prev_text || 'Prev';
+  // var nextText = options.next_text || 'Next';
+  // var prevNext = options.hasOwnProperty('prev_next') ? options.prev_next : true;
+  // var transform = options.transform;
+  // var self = this;
+  // var result = '';
+  // var i;
+  //
+  // if (!current) return '';
+  //
+  // var currentPage = '<span class="page-number current">' +
+  //   (transform ? transform(current) : current) +
+  //   '</span>';
+  //
+  // function link(i) {
+  //   return i === 1 ? base : base + format.replace('%d', i);
+  // }
+  //
+  // function pageLink(i) {
+  //   console.log('页数',i);
+  //   return '<a class="page-number" href="' + link(i) + '">' +
+  //     (transform ? transform(i) : i) +
+  //     '</a>';
+  // }
+  //
+  // // Display the link to the previous page（显示上一页的链接）
+  // if (prevNext && current > 1) {
+  //   result += '<a class="extend prev" rel="prev" href="' + link(current - 1) + '">' + prevText + '</a>';
+  // }
+  //
+  // if (options.show_all) {
+  //   // Display pages on the left side of the current page（在当前页面左侧显示页面）
+  //   for (i = 1; i < current; i++) {
+  //     result += pageLink(i);
+  //   }
+  //
+  //   // Display the current page（显示当前页面）
+  //   result += currentPage;
+  //
+  //   // Display pages on the right side of the current page（在当前页面右侧显示页面）
+  //   for (i = current + 1; i <= total; i++) {
+  //     result += pageLink(i);
+  //   }
+  // } else {
+  //   // It's too complicated. May need refactor.
+  //   var leftEnd = current <= endSize ? current - 1 : endSize;
+  //   var rightEnd = total - current <= endSize ? current + 1 : total - endSize + 1;
+  //   var leftMid = current - midSize <= endSize ? current - midSize + endSize : current - midSize;
+  //   var rightMid = current + midSize + endSize > total ? current + midSize - endSize : current + midSize;
+  //   var spaceHtml = '<span class="space">' + space + '</span>';
+  //   console.log('leftEnd:',leftEnd);
+  //   console.log('rightEnd:',rightEnd);
+  //   console.log('leftMid:',leftMid);
+  //   console.log('rightMid:',rightMid);
+  //   console.log('spaceHtml:',spaceHtml);
+  //
+  //   // Display pages on the left edge
+  //   for (i = 1; i <= leftEnd; i++) {
+  //     result += pageLink(i);
+  //   }
+  //
+  //   // Display spaces between edges and middle pages
+  //   if (space && current - endSize - midSize > 1) {
+  //     result += spaceHtml;
+  //   }
+  //
+  //   // Display left middle pages
+  //   if (leftMid > leftEnd) {
+  //     for (i = leftMid; i < current; i++) {
+  //       result += pageLink(i);
+  //     }
+  //   }
+  //
+  //   // Display the current page
+  //   result += currentPage;
+  //
+  //   // Display right middle pages
+  //   if (rightMid < rightEnd) {
+  //     for (i = current + 1; i <= rightMid; i++) {
+  //       result += pageLink(i);
+  //     }
+  //   }
+  //
+  //   // Display spaces between edges and middle pages
+  //   if (space && total - endSize - midSize > current) {
+  //     result += spaceHtml;
+  //   }
+  //
+  //   // Dispaly pages on the right edge
+  //   for (i = rightEnd; i <= total; i++) {
+  //     result += pageLink(i);
+  //   }
+  // }
+  //
+  // // Display the link to the next page
+  // if (prevNext && current < total) {
+  //   result += '<a class="extend next" rel="next" href="' + link(current + 1) + '">' + nextText + '</a>';
+  // }
+  //
+  // return result;
   options = options || {};
 
   var current = options.current || 0;
   var total = options.total || 1;
-  var endSize = options.hasOwnProperty('end_size') ? +options.end_size : 2;
+  var endSize = options.hasOwnProperty('end_size') ? +options.end_size : 1;
   var midSize = options.hasOwnProperty('mid_size') ? +options.mid_size : 2;
   var space = options.hasOwnProperty('space') ? options.space : '&hellip;';
   var base = options.base || '';
@@ -21,8 +130,8 @@ function paginatorHelper(options) {
   if (!current) return '';
 
   var currentPage = '<span class="page-number current">' +
-    (transform ? transform(current) : current) +
-    '</span>';
+      (transform ? transform(current) : current) +
+      '</span>';
 
   function link(i) {
     return i === 1 ? base : base + format.replace('%d', i);
@@ -31,75 +140,47 @@ function paginatorHelper(options) {
   function pageLink(i) {
     console.log('页数',i);
     return '<a class="page-number" href="' + link(i) + '">' +
-      (transform ? transform(i) : i) +
-      '</a>';
+        (transform ? transform(i) : i) +
+        '</a>';
   }
 
   // Display the link to the previous page（显示上一页的链接）
   if (prevNext && current > 1) {
     result += '<a class="extend prev" rel="prev" href="' + link(current - 1) + '">' + prevText + '</a>';
   }
-
-  if (options.show_all) {
-    // Display pages on the left side of the current page（在当前页面左侧显示页面）
-    for (i = 1; i < current; i++) {
+  for (var i=1; i<total; i++){
+    // 第一页
+    if (i == 1){
       result += pageLink(i);
     }
 
-    // Display the current page（显示当前页面）
-    result += currentPage;
-
-    // Display pages on the right side of the current page（在当前页面右侧显示页面）
-    for (i = current + 1; i <= total; i++) {
-      result += pageLink(i);
-    }
-  } else {
-    // It's too complicated. May need refactor.
-    var leftEnd = current <= endSize ? current - 1 : endSize;
-    var rightEnd = total - current <= endSize ? current + 1 : total - endSize + 1;
-    var leftMid = current - midSize <= endSize ? current - midSize + endSize : current - midSize;
-    var rightMid = current + midSize + endSize > total ? current + midSize - endSize : current + midSize;
-    var spaceHtml = '<span class="space">' + space + '</span>';
-    console.log('leftEnd:',leftEnd);
-    console.log('rightEnd:',rightEnd);
-    console.log('leftMid:',leftMid);
-    console.log('rightMid:',rightMid);
-    console.log('spaceHtml:',spaceHtml);
-
-    // Display pages on the left edge
-    for (i = 1; i <= leftEnd; i++) {
-      result += pageLink(i);
-    }
-
-    // Display spaces between edges and middle pages
-    if (space && current - endSize - midSize > 1) {
+    // 省略页
+    if (i == 2 && current - 3 > i){
       result += spaceHtml;
     }
 
-    // Display left middle pages
-    if (leftMid > leftEnd) {
-      for (i = leftMid; i < current; i++) {
-        result += pageLink(i);
-      }
+    // 前几页
+    if (i >= current - 3){
+      result += pageLink(i);
     }
 
-    // Display the current page
-    result += currentPage;
-
-    // Display right middle pages
-    if (rightMid < rightEnd) {
-      for (i = current + 1; i <= rightMid; i++) {
-        result += pageLink(i);
-      }
+    // 当前页
+    if (i == current){
+      result += currentPage;
     }
 
-    // Display spaces between edges and middle pages
-    if (space && total - endSize - midSize > current) {
+    // 后几页
+    if (i <= current + 3){
+      result += pageLink(i);
+    }
+
+    // 省略页
+    if (i == total-1 && current + 3 < i){
       result += spaceHtml;
     }
 
-    // Dispaly pages on the right edge
-    for (i = rightEnd; i <= total; i++) {
+    // 最后一页
+    if (i == total){
       result += pageLink(i);
     }
   }
